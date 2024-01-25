a0eea39f206a00ec79dc01951b96a107f5b56c1c
aelys <aelysch@proton.me>
2024-01-24 22:50:05 -0300

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <>
+            <div className={"bg-black flex flex-col justify-center items-center h-screen"}>
+                <div className={"flex flex-col justify-center items-center"}>
+                    <h1 className={"text-4xl font-bold text-gray-900"}>Aelys.me</h1>
+                    <h2 className={"text-2xl font-semibold text-gray-900"}>Aelys' website</h2>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/components/Home.tsx
@@ -0,0 +1,27 @@
+export default function Home () {
+ 
+    return (
+            <div className={"relative ml-auto mr-auto max-w-[80ch] scroll-smooth p-2 bg-black"} style={{ minHeight: 'calc(100vh - 1000px)'}}>
+               <main className="overflow-x-hidden px-7 py-10">
+                <div className="prose prose-invert m-automb-8">
+                    <h1 className="text-4xl font-bold text-white mb-0 font-kurale">Aelys</h1>
+                    <p className="text-white pt-4">
+                    Howdy! I am [redacted] (aka Aelys), a self-taught developer. I love Linux, privacy &amp; open-source software. I also like shitposting lol.
+                    </p>
+                    <p className="text-white pt-4">
+                    When I'm not programming, I like to play games, listen to music, watch movies/series and spend time with friends.
+                    </p>
+                    <p className="text-white pt-4">
+                    Best way to reach out to me is via Discord @A3lys.
+                        </p>
+                    <p className="flex text-white underline pt-2 font-bold flex-wrap gap-3">
+                        <a target="_blank" href="https://github.com/LysImbecile/">GitHub</a>
+                        <a target="_blank" href="https://www.youtube.com/channel/UCC82bHCIS667v3loXsPd5MA">Youtube</a>
+                    </p>
+                 
+                </div>
+               </main>
+            </div>
+      
+    );
+}
