25bbc50451fa437d8570edb327868da01c478a47
aelys <aelysch@proton.me>
2024-01-27 17:32:17 -0300

    Fix link in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
                                 </div>
                                 {isDropdownOpen && (
                                     <div className="absolute p-2 left-0 mt-2 w-72 rounded-md shadow-lg bg-stone-950 border border-gray-600 text-black">
-                                        <a href="https://cv.aelys.me/" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-zinc-800">
+                                        <a href="https://cv.aelys.me" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-zinc-800">
                                             <div className='text-sm font-medium leading-none text-white'>Curriculum - 📖</div>
                                             <p className='line-clamp-2 text-sm leading-snug text-muted-foreground text-white'>My personal CV with full info about my person.</p>
                                         </a>
