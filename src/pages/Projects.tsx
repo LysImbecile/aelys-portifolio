311c1e1c5e77a7b183fb840a06befd186890be11
henriquejustee <aelysch@proton.me>
2024-02-18 00:10:33 -0300

    Update Projects page layout and links

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,18 +2,18 @@ export default function Projects() {
     return (
         <div className={"relative ml-auto mr-auto  scroll-smooth p-2 bg-black"} style={{ minHeight: 'calc(100vh - 1000px)' }}>
             <section className="md:h-full flex items-center text-gray-600">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="text-center mb-12">
-                        <h5 className="text-base underline md:text-lg text-red-600 mb-1">See all my projects here</h5>
+                <div className="container px-5 py-14 mx-auto">
+                    <div className="text-center mb-14">
+                        <a href="https://github.com/henriquejustee?tab=repositories" className="text-base underline md:text-lg cursor-pointer transition duration-200 hover:text-red-400 text-red-600 mb-1">See all my projects here</a>
                         <h1 className="text-4xl md:text-6xl text-white font-semibold">My recent projects</h1>
                     </div>
                     <div className="flex flex-wrap -m-4">
                         {/* Repeat this div for each card */}
                         <div className="p-4 sm:w-1/2 lg:w-1/3">
-                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <div className="border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img className="lg:h-72 md:h-48 w-full object-cover object-center"
                                     src="https://i.ibb.co/SdjZrxy/image.png" alt="blog" />
-                                <div className="p-6 h-full hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
+                                <div className="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
                                     <h2 className="text-base font-medium text-indigo-300 mb-1">October 29, 2021</h2>
                                     <h1 className="text-2xl font-semibold mb-3">Github Finder</h1>
                                     <p className="leading-relaxed mb-3 line-clamp-3"> This React frontend application integrates with the GitHub API, allowing you to search for users and view their basic profile information. A future route will display a grid of each user's public repositories.</p>
@@ -46,10 +46,10 @@ export default function Projects() {
                             </div>
                         </div>
                         <div className="p-4 sm:w-1/2 lg:w-1/3">
-                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <div className="border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img className="lg:h-72 md:h-48 w-full object-cover object-center"
                                     src="https://i.ibb.co/tw0RqR3/weatherbot.png" alt="blog" />
-                                <div className="p-6 h-full hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
+                                <div className="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
                                     <h2 className="text-base font-medium text-indigo-300 mb-1">October 29, 2021</h2>
                                     <h1 className="text-2xl font-semibold mb-3">Weather Discord Bot</h1>
                                     <p className="leading-relaxed mb-3 line-clamp-3">Discord Bot integrated with a Weather public API, check your local city weather with simple slashcommands, built with Node, DiscordJS and Typescript.</p>
@@ -82,10 +82,10 @@ export default function Projects() {
                             </div>
                         </div>
                         <div className="p-4 sm:w-1/2 lg:w-1/3">
-                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <div className="border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img className="lg:h-72 md:h-48 w-full object-cover object-center"
                                     src="https://i.ibb.co/DtB7nLt/297572084-9ca6f606-bf3f-4a6a-a8d1-f16ed382aca4.png" alt="blog" />
-                                <div className="p-6 hover:bg-indigo-700 h-full hover:text-white transition duration-300 ease-in">
+                                <div className="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
                                     <h2 className="text-base font-medium text-indigo-300 mb-1">October 29, 2021</h2>
                                     <h1 className="text-2xl font-semibold mb-3">Auto Certification system</h1>
                                     <p className="leading-relaxed mb-3 line-clamp-3">Automation alowwing a fast course certification generation with a simple Excel sheet, built with Python and Canva library to generate texts on certifications models.</p>
