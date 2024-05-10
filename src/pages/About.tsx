67607fecb4f953afa508f025c56b039c69e235db
aelys <aelysch@proton.me>
2024-05-10 16:44:16 -0300

    feat: Update About page with basic info and what I like to do

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,3 @@
-import { TextRevealCard,
-    TextRevealCardDescription,
-    TextRevealCardTitle, } from '../components/ui/text-reveal-card';
-
 export default function About() {
     return (
         <>
@@ -14,26 +10,15 @@ export default function About() {
                    
                     </div>
                     <section className="pl-5">
-        <div>
-                    <TextRevealCard className='w-full'
-        text="Who am i?"
-        revealText="Who are you?"
-      >
-        <TextRevealCardTitle>
-        Basic Info
-        </TextRevealCardTitle>
-        
-        <TextRevealCardDescription>
-        Ayo! My real name is <span className="text-red-600">[ Henri ]</span> and i'm a dude with <span className="text-red-600">[ 17 ]</span> years old.
+
+                    <h2 className={"text-2xl pt-5 font-normal underline decoration-indigo-500 underline-offset-4 text-white"}><span>Basic Info</span></h2>
+                        <p className="text-white pt-2 font-light text-wrap">   Ayo! My real name is <span className="text-red-600">[ Henri ]</span> and i'm a dude with <span className="text-red-600">[ 17 ]</span> years old.
                            A person who loves freedom, possibilities and creativity, these few things give me more reasons to appreciate the beauty of life.
                            Actually i'm a focused learner, extracting knowledge from leafs of the internet, looking for a nice and cold bottle of coke to enjoy and think about the future.
-        </TextRevealCardDescription>
-
-        
-      </TextRevealCard>
-      </div>
-
-                  
+                        </p>  
+        <div>
+                    
+      </div>  
                    
                         <h2 className={"text-2xl pt-10 font-normal underline decoration-indigo-500 underline-offset-4 text-white"}><span>What i like to do</span></h2>
                         <p className="text-white pt-2 font-light text-wrap"> I like to spend a good time thinking, planning my routine. I don't play games for many hours like before, but i always entretein myself with some good games. I like to explore my Spotify, maybe someday i will have a decent playlist to share with everyone. Coding is my favorite hobby, at this time i am learning more about low level programming, Golang, Rust and relevant I.T stuff.
